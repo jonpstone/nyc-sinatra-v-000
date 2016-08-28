cc6a6a0e0f95ad957b1623bdd572d572d413c966
Jonathan Stone <jon_p_stone@yahoo.com>
2016-08-28 03:12:50 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,4 +1,5 @@
 class FiguresController < ApplicationController
+
   get '/figures' do
     @figures = Figure.all
     erb :'figures/index'
@@ -11,12 +12,19 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
+      #Ask if the figure hash's 'name value' is empty, if it is the 'else' condition will redirect.
     if !params[:figure][:name].empty?
       @figure = Figure.find_or_create_by(name: params[:figure][:name])
+
+      #Add landmarks to the figure hash, if figure has 'landmark_id' key and that the value to that key is empty.
       params[:figure][:landmark_ids].each{|id| @figure.landmarks << Landmark.find(id)} if params[:figure].has_key?("landmark_ids") && !params[:figure][:landmark_ids].empty?
       @figure.landmarks << Landmark.find_or_create_by(params[:landmark]) if !params[:landmark][:name].empty?
+
+      #Add title to figure hash, if figure has a title_id key and it is empty.
       params[:figure][:title_ids].each{|id| @figure.titles << Title.find(id)} if params[:figure].has_key?("title_ids") && !params[:figure][:title_ids].empty?
       @figure.titles << Title.find_or_create_by(params[:title]) if !params[:title][:name].empty?
+
+      #Save and redirect to created
       @figure.save
       redirect "/figures/#{@figure.id}"
     else
@@ -39,10 +47,16 @@ class FiguresController < ApplicationController
   post '/figures/:id' do
     @figure = Figure.find(params[:id])
     @figure.update(params[:figure])
+
+    #Add landmarks to the figure hash, if figure has 'landmark_id' key and that the value to that key is empty.
     params[:figure][:landmark_ids].each{|id| @figure.landmarks << Landmark.find(id)} if params[:figure].has_key?("landmark_ids") && !params[:figure][:landmark_ids].empty?
     @figure.landmarks << Landmark.find_or_create_by(params[:landmark]) if !params[:landmark][:name].empty?
+
+    #Add title to figure hash, if figure has a title_id key and it is empty.
     params[:figure][:title_ids].each{|id| @figure.titles << Title.find(id)} if params[:figure].has_key?("title_ids") && !params[:figure][:title_ids].empty?
     @figure.titles << Title.find_or_create_by(params[:title]) if !params[:title][:name].empty?
+
+    #Save and redirect to created
     @figure.save
     redirect "/figures/#{@figure.id}"
   end
